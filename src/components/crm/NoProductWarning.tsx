96d8e93d38811836b75af5ba88609d0ac2094507
Tobi Okanlawon <tobiinlondon34@gmail.com>
2026-08-18 12:30:19 +0100

    push some code

---
+++ src/components/crm/NoProductWarning.tsx
@@ -0,0 +1,9 @@
+export default function NoProductWarning() {
+  return (
+    <>
+      <p className="text-sm text-gray-400 text-center py-4">
+        You must create a product before creating orders
+      </p>
+    </>
+  );
+}
